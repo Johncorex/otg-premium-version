6369594517bc679a2ca3e3d4710db195c68ffe14
Erick Nunes <erickzenho@live.com>
2020-04-04 01:48:47 -0300

    Update toolgear.lua

--- data/actions/scripts/tools/toolgear.lua
+++ data/actions/scripts/tools/toolgear.lua
@@ -1,4 +1,5 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	if math.random(100) > 5 then
 	return onUseRope(player, item, fromPosition, target, toPosition, isHotkey)
 		or onUseShovel(player, item, fromPosition, target, toPosition, isHotkey)
 		or onUsePick(player, item, fromPosition, target, toPosition, isHotkey)
@@ -7,4 +8,10 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		or onUseSpoon(player, item, fromPosition, target, toPosition, isHotkey)
 		or onUseScythe(player, item, fromPosition, target, toPosition, isHotkey)
 		or onUseKitchenKnife(player, item, fromPosition, target, toPosition, isHotkey)
+	else
+		player:say("Oh no! Your tool is jammed and can't be used for a minute.", TALKTYPE_MONSTER_SAY)
+		item:transform(item.itemid + 1)
+		item:decay()
+	end
+	return true
 end
