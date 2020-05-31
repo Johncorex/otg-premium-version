07c14f2b37eaa72c1663cb0767b6aa02e402343a
Johncore <gjohnes2018@gmail.com>
2020-05-31 02:04:36 -0300

    [FIX] Speed all monsters

--- src/monsters.cpp
+++ src/monsters.cpp
@@ -799,7 +799,7 @@ MonsterType* Monsters::loadMonster(const std::string& file, const std::string& m
 	}
 
 	if ((attr = monsterNode.attribute("speed"))) {
-		mType->info.baseSpeed = pugi::cast<int32_t>(attr.value()) * 2.55;
+		mType->info.baseSpeed = pugi::cast<int32_t>(attr.value()) * 1.10;
 	}
 
 	if ((attr = monsterNode.attribute("manacost"))) {
