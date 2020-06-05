8126f4c8867344677c4b054ede67817ef82a1f68
lmiguelm26 <52018940+lmiguelm26@users.noreply.github.com>
2020-06-05 16:30:19 -0500

    fix IMBUEMENT - MANA LEECH AND  LIFE LEECH - LM26
    fix bug #44 Pelase should be reviewed !!

--- src/game.cpp
+++ src/game.cpp
@@ -4497,7 +4497,7 @@ bool Game::combatChangeHealth(Creature* attacker, Creature* target, CombatDamage
 				CombatDamage tmpDamage;
 				tmpDamage.origin = ORIGIN_SPELL;
 				tmpDamage.primary.type = COMBAT_HEALING;
-				tmpDamage.primary.value = std::round((realDamage * (0.9 + (damage.affected / 10.)) * (attackerPlayer->getSkillLevel(SKILL_LIFE_LEECH_AMOUNT) /100.)) / damage.affected);
+				tmpDamage.primary.value = std::round(realDamage * (attackerPlayer->getSkillLevel(SKILL_LIFE_LEECH_AMOUNT) /100.));
 
 				Combat::doCombatHealth(nullptr, attackerPlayer, tmpDamage, tmpParams);
 			}
@@ -4508,7 +4508,7 @@ bool Game::combatChangeHealth(Creature* attacker, Creature* target, CombatDamage
 				CombatDamage tmpDamage;
 				tmpDamage.origin = ORIGIN_SPELL;
 				tmpDamage.primary.type = COMBAT_MANADRAIN;
-				tmpDamage.primary.value = std::round((realDamage * (0.9 + (damage.affected / 10.)) * (attackerPlayer->getSkillLevel(SKILL_MANA_LEECH_AMOUNT) /100.)) / damage.affected);
+				tmpDamage.primary.value = std::round(realDamage * (attackerPlayer->getSkillLevel(SKILL_MANA_LEECH_AMOUNT) /100.));
 
 				Combat::doCombatMana(nullptr, attackerPlayer, tmpDamage, tmpParams);
 			}
@@ -5027,7 +5027,7 @@ void Game::applyImbuementEffects(Creature * attacker, CombatDamage & damage, int
 			CombatDamage tmpDamage;
 			tmpDamage.origin = ORIGIN_SPELL;
 			tmpDamage.primary.type = COMBAT_HEALING;
-			tmpDamage.primary.value = std::round((realDamage * (0.9 + (damage.affected / 10.)) * (attackerPlayer->getSkillLevel(SKILL_LIFE_LEECH_AMOUNT) / 100.)) / damage.affected);
+			tmpDamage.primary.value = std::round(realDamage * (attackerPlayer->getSkillLevel(SKILL_LIFE_LEECH_AMOUNT) / 100.));
 
 			Combat::doCombatHealth(nullptr, attackerPlayer, tmpDamage, tmpParams);
 		}
@@ -5038,7 +5038,7 @@ void Game::applyImbuementEffects(Creature * attacker, CombatDamage & damage, int
 			CombatDamage tmpDamage;
 			tmpDamage.origin = ORIGIN_SPELL;
 			tmpDamage.primary.type = COMBAT_MANADRAIN;
-			tmpDamage.primary.value = std::round((realDamage * (0.9 + (damage.affected / 10.)) * (attackerPlayer->getSkillLevel(SKILL_MANA_LEECH_AMOUNT) / 100.)) / damage.affected);
+			tmpDamage.primary.value = std::round(realDamage * (attackerPlayer->getSkillLevel(SKILL_MANA_LEECH_AMOUNT) / 100.));
 
 			Combat::doCombatMana(nullptr, attackerPlayer, tmpDamage, tmpParams);
 		}
